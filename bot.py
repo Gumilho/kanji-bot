149c6efedc6f366b0f4fc3fd97e3daf2a7a107b4
Tafarel Yan <tafarel.yan@gmail.com>
2019-04-21 17:00:30 -0300

    Eh

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+async def bgtask():
+	counter = 0
+	channel = client.get_guild(493580129025654785).get_channel(493580129025654787)
+	while True:
+        	counter += 1
+        	await channel.send(counter)
+        	await asyncio.sleep(60)
+
+@client.event
+async def on_ready():
+	print("ready!")
+	client.loop.create_task(bgtask())
+	await client.change_presence(activity=discord.Game(name="Weeb"))
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	else:
+		await message.channel.send("Hi")
+
+client.run("NTY5NTY0NjcwODg5ODIwMTYw.XLyelw.L8UfCWLheY-5f4uGXpwqnYXYe0c")
