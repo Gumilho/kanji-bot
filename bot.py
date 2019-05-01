dadc966d5c4275dee60cd55c749a2bdb61594779
Gumilho <doraniel2000@gmail.com>
2019-05-01 17:34:59 -0300

    v1.1.18

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode = 'require')
 c = conn.cursor()
 c.execute("create table hello (a int, b int)")
-c.execute("insert into hello (a, b),(1, 2)")
+c.execute("insert into hello (a, b) values (1, 2)")
 c.execute("select * from hello")
 print(c.cursor.fetchall())
 client = discord.Client()
